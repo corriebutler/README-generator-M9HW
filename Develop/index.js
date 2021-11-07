48f2d26ce264cdf05a438e8b752b1a79e96388c9
Corrie Butler <corriebutler12@gmail.com>
2021-11-07 14:53:30 -0700

    added first question and was able to get it to log in the console

--- Develop/index.js
+++ Develop/index.js
@@ -3,18 +3,36 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+];
+    
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-fs.writeFile('README.md',dataPlaceHolder), err => {
-    if(err) throw err;
 
-    console.log('README was now generated. Checkout README.md to the see output!');
-};
-
-// TODO: Create a function to initialize app
-function init() {}
+// Generate the actual README file that pulls data
+// fs.writeFile('README.md',dataPlaceHolder), err => {
+    //     if(err) throw err;
+    
+    //     console.log('README was now generated. Checkout README.md to the see output!');
+    // };
+    
+    // TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => console.log(answers))
+}
 
 // Function call to initialize app
 init();
