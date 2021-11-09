ccf879ebd8f7c126c446769c5e9bea9f1d828c12
Corrie Butler <corriebutler12@gmail.com>
2021-11-09 14:19:20 -0700

    added the rest of the questions

--- Develop/index.js
+++ Develop/index.js
@@ -12,12 +12,81 @@ const questions = [
             if (titleInput) {
                 return true;
             } else {
+                console.log(`Please enter a valid Project Title!`);
                 return false;
             }
         }
-    }
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Give a description of your project (Required):',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                console.log(`Please enter a valid project description!`);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'projectLicense',
+        message: 'What license type would you like to assign to this project?',
+        choices: ['Apache 2.0', 'GPLv2','GLPv3','MIT','None'],        
+        validate: licenseInput => {
+            if (licenseInput) {
+                console.log(`Please select one of the license types. If you do not wish to apply a license type, please select "None".`);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectUsage',
+        message: 'What will this project be used for?',
+    },
+    {
+        type: 'input',
+        name: 'projectContribution',
+        message: 'What is needed for someone to contribute to the project?',
+    },
+    {
+        type: 'input',
+        name: 'projectInstallation',
+        message: 'What are the steps to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'githubUser',
+        message: 'What is your GitHub username? (Required):',
+        validate: githubInput => {
+            if (githubInput) {
+                console.log(`Please enter your Github Username!`);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectQuestions',
+        message: 'What is your best email for contact on this project? (Required):',
+        validate: questionsInput => {
+            if (questionsInput) {
+                console.log(`Please enter a valid email address!`);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
 ];
-    
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
