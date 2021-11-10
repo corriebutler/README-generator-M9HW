fe54a05140415910f8074810918ea916ba4e058b
Corrie Butler <corriebutler12@gmail.com>
2021-11-09 20:57:14 -0700

    got the write to file written in the function init, received an error when running it

--- Develop/index.js
+++ Develop/index.js
@@ -90,12 +90,12 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     // Generate the actual README file that pulls data
-    fs.writeFile(fileName,data, err => {
-            if(err) {
-                console.log(err);;
-            };
-            console.log('README was now generated. Checkout README.md to the see output!');
-        });
+    // fs.writeFile(fileName, data, err => {
+    //         if(err) {
+    //             console.log(err);;
+    //         };
+    //         console.log('README was now generated. Checkout README.md to the see output!');
+    //     });
 }
 
 
@@ -103,7 +103,15 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-        .then(answers => console.log(answers))
+        .then (answers => console.log(answers))
+        .then (answers => {
+            writeToFile('../dist/README.md', data, err => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log('Your inputs have been written to a new README file. Check the dist folder to find the README.md file you created!');
+            })
+        })
 }
 
 // Function call to initialize app
